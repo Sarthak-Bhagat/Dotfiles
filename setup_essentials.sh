a664b0cd914fe81b83163daeefb8cf24394b3512
Sarthak Bhagat <sarth.bhagat@gmail.com>
2022-01-18 19:39:58 +0530

    added font

--- setup_essentials.sh
+++ setup_essentials.sh
@@ -1,6 +1,6 @@
 ln -s ~/Pictures/wallpapers ~/.config/leftwm/themes/current/backgrounds
 
-paru -S exa bat python-virtualenvwrapper waterfox-g4-bin neovim-nightly-bin
+paru -S exa bat python-virtualenvwrapper waterfox-g4-bin neovim-nightly-bin nerd-fonts-source-code-pro
 
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 
