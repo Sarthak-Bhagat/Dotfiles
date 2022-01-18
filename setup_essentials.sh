e47c63b0e43e8bfac0135d4995c13b5b7ded7841
Sarthak Bhagat <sarth.bhagat@gmail.com>
2022-01-18 19:35:22 +0530

    First commit

---
+++ setup_essentials.sh
@@ -0,0 +1,10 @@
+ln -s ~/Pictures/wallpapers ~/.config/leftwm/themes/current/backgrounds
+
+paru -S exa bat python-virtualenvwrapper waterfox-g4-bin neovim-nightly-bin
+
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+
+rm -f ~/setup_essentials.sh
+rm -f ~/LICENSE
+
+echo "Run doom install to install emacs"
