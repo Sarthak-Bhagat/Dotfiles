bc32d043a0dc46d225ebd7686375fad0ad62e895
Sarthak-Bhagat <sarth.bhagat@gmail.com>
2024-02-18 20:20:10 +0530

    update

--- setup_essentials.sh
+++ setup_essentials.sh
@@ -1,10 +1,7 @@
-ln -s ~/Pictures/wallpapers ~/.config/leftwm/themes/current/backgrounds
+paru -S exa bat neovim-nightly-bin nerd-fonts-source-code-pro fish
 
-paru -S exa bat python-virtualenvwrapper waterfox-g4-bin neovim-nightly-bin nerd-fonts-source-code-pro
-
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
 rm -f ~/setup_essentials.sh
 rm -f ~/LICENSE
 
-echo "Run doom install to install emacs"
